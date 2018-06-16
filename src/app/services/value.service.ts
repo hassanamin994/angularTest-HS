469bce550410a188dc7cba21a1fdcb12c230e8a2
hassanamin994 <hassanmamin994@gmail.com>
2018-06-17 01:48:59 +0200

    Add value service

---
+++ src/app/services/value.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { Subject }    from 'rxjs';
+
+@Injectable()
+export class ValueService {
+
+  private _data = new Subject<Object>();
+  private _dataStream = this._data.asObservable();
+  private _subscriptions: Map<string, Array<Function>> = new Map<string, Array<Function>>();
+
+  private _minValue: number = 0;
+  private _maxValue: number = 100;
+  private _step: number = 1;
+
+  constructor() {
+    this._dataStream.subscribe((data) => this._onEvent(data));
+    // intitialize value 
+    this.notifyDataChanged('value', 0);
+  }
+
+  get minValue() {
+    return this._minValue;
+  }
+
+  get maxValue() {
+    return this._maxValue;
+  }
+
+  get step() {
+    return this._step;
+  }
+
+  notifyDataChanged(event, value) {
+    let current = this._data[event];
+    if (current !== value) {
+      this._data[event] = value;
+
+      this._data.next({
+        event: event,
+        data: this._data[event]
+      });
+    }
+  }
+
+  subscribe(event: string, callback: Function) {
+    let subscribers = this._subscriptions.get(event) || [];
+    subscribers.push(callback);
+    this._subscriptions.set(event, subscribers);
+  }
+
+  _onEvent(data: any) {
+    let subscribers = this._subscriptions.get(data['event']) || [];
+    subscribers.forEach((callback) => {
+      callback.call(null, data['data']);
+    });
+  }
+}
